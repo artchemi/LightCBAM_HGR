3d2026af5837852928553b6e506f456af9a3073d
artchemi <art.dan08@yandex.ru>
2025-07-03 13:14:21 +0800

    ADD: metrics

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import random
 import numpy as np
 import tensorflow as tf
+from sklearn.metrics import f1_score, confusion_matrix, classification_report
 
 
 def set_seed(seed=42):
@@ -19,4 +20,14 @@ def set_seed(seed=42):
         tf.config.experimental.enable_op_determinism()
     except AttributeError:
         pass
+
+
+def evaluate_metrics(model: tf.keras.Sequential, emg: np.ndarray, labels_true: np.ndarray):
+
+    labels_preds = model.predict(emg)
+
+    f1_score_test = f1_score(labels_true, labels_preds, average='macro', zero_division=0)
+    classification_report_test = classification_report(labels_true, labels_preds, zero_division=0)
+
+    return f1_score_test, classification_report_test
     
