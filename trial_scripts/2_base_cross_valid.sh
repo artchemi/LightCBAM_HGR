57f187beb0fe337b6dbb83c7d06dc3e627184fc4
artchemi <art.dan08@yandex.ru>
2025-07-17 09:30:12 +0800

    Minors

--- trial_scripts/2_base_cross_valid.sh
+++ trial_scripts/2_base_cross_valid.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 scripts=(
-  "python src/train_kfold.py --window_size 24 --mode 'base'"
-  "python src/train_kfold.py --window_size 28 --mode 'base'"
-  "python src/train_kfold.py --window_size 32 --mode 'base'"
-  "python src/train_kfold.py --window_size 36 --mode 'base'"
-  "python src/train_kfold.py --window_size 40 --mode 'base'"
-  "python src/train_kfold.py --window_size 44 --mode 'base'"
-  "python src/train_kfold.py --window_size 48 --mode 'base'"
-  "python src/train_kfold.py --window_size 52 --mode 'base'"
+  # "python src/train_kfold.py --window_size 24 --mode 'base'"
+  # "python src/train_kfold.py --window_size 28 --mode 'base'"
+  # "python src/train_kfold.py --window_size 32 --mode 'base'"
+  # "python src/train_kfold.py --window_size 36 --mode 'base'"
+  # "python src/train_kfold.py --window_size 40 --mode 'base'"
+  # "python src/train_kfold.py --window_size 44 --mode 'base'"
+  # "python src/train_kfold.py --window_size 48 --mode 'base'"
+  # "python src/train_kfold.py --window_size 52 --mode 'base'"
   "python src/train_kfold.py --window_size 56 --mode 'base'"
   "python src/train_kfold.py --window_size 60 --mode 'base'"
   "python src/train_kfold.py --window_size 64 --mode 'base'"
